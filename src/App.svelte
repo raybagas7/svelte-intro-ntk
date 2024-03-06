6871c9efe5e7fd2ac17ca89d22bb3582c9e18c2a
raybagas7 <raysam.gas@gmail.com>
2024-03-06 20:12:30 +0700

    feat(root component): - experience how state works in svelte and change the state variable

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	let name = "Ray";
+	let beltColour = "yellow"
+	const handleClick = () => {
+		beltColour = 'red'
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Hi Agas!</p>
+	<p>{beltColour} belt</p>
+	<button on:click={handleClick}>Update belt colour</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
