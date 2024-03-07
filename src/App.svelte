77a47cab102f46342fafcf2f8e1046fd4d06d472
raybagas7 <raysam.gas@gmail.com>
2024-03-07 14:22:09 +0700

    feat(Inline Event Handlers): - make an inline event handler and prevent it to auto invoke by using inline function

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
 		{name: 'Samudra', beltColor: "Red", age: 16, id:2},
 		{name: 'Bagas', beltColor: "Purple", age: 18, id:3}
 	]
+
+	const handleDelete = (id) => {
+		// delete person from people
+		people = people.filter((person) => person.id !== id)
+	}
 </script>
 
 <main>
@@ -11,6 +16,7 @@
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColor} belt.</p>
+			<button on:click={() => handleDelete(person.id)}>Delete</button>
 		</div>
 	{:else}
 		<p>There are no people to show...</p>
